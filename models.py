bdc91743a55c02d93211d3f29d822126a3a7b996
mohamed samir <56839321+mohamedsamiromar@users.noreply.github.com>
2022-05-27 19:58:39 +0200

    Feat User model and flask login and requirements file

--- models.py
+++ models.py
@@ -1,15 +1,24 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_migrate import Migrate
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 db = SQLAlchemy(app)
 
 
-class User(db.Model):
+from flask_login import UserMixin
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = "user"
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    pwd = db.Column(db.String(300), nullable=False, unique=True)
 
     def __repr__(self):
         return '<User %r>' % self.username

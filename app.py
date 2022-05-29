289ca4870cc05ab63afcca853a85e3ab688a2a1d
mohamed samir <56839321+mohamedsamiromar@users.noreply.github.com>
2022-05-29 21:25:56 +0200

    migrate

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 from flask import Flask
+
 app = Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy(app)
+from flask_migrate import Migrate
+
+migrate = Migrate(app, db)
 
 
 @app.route('/')
--- models.py
+++ models.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from flask_login import UserMixin
 
@@ -29,6 +31,3 @@ class DailyExercise(db.Model):
 
     def __repr__(self):
         return '<DailyExercise %r>' % self.exercise_name
-
-
-

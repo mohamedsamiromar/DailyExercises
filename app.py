86ff3a8f1620a666112b135a6f55db20a12458e1
mohamed samir <56839321+mohamedsamiromar@users.noreply.github.com>
2022-05-29 22:10:13 +0200

    register form

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask
-
+from flask import Flask, render_template, request
+from flask_login import login_user, current_user, logout_user, login_required
 app = Flask(__name__)
 from flask_sqlalchemy import SQLAlchemy
 
@@ -8,10 +8,40 @@ from flask_migrate import Migrate
 
 migrate = Migrate(app, db)
 
+import MySQLdb.cursors
+import re
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+
+# @app.route('/')
+# def hello_world():
+#     return 'Hello World!'
+
+
+# @app.route('/register', methods=['GET', 'POST'])
+# def register():
+#     msg = ''
+#     if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+#         username = request.form['username']
+#         password = request.form['password']
+#         email = request.form['email']
+#         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+#         cursor.execute('SELECT * FROM accounts WHERE username = % s', (username,))
+#         account = cursor.fetchone()
+#         if account:
+#             msg = 'Account already exists !'
+#         elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+#             msg = 'Invalid email address !'
+#         elif not re.match(r'[A-Za-z0-9]+', username):
+#             msg = 'Username must contain only characters and numbers !'
+#         elif not username or not password or not email:
+#             msg = 'Please fill out the form !'
+#         else:
+#             cursor.execute('INSERT INTO accounts VALUES (NULL, % s, % s, % s)', (username, password, email,))
+#             mysql.connection.commit()
+#             msg = 'You have successfully registered !'
+#     elif request.method == 'POST':
+#         msg = 'Please fill out the form !'
+#     return render_template('register.html', msg=msg)
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -1,13 +1,22 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import  LoginManager
 from flask_migrate import Migrate
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-
 from flask_login import UserMixin
+
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
 class User(UserMixin, db.Model):
@@ -16,6 +25,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(25), nullable=False)
 
     def __repr__(self):
         return '<User %r>' % self.username

449c5e6d7f054be070eebe111c1512e49d036891
mohamed samir <56839321+mohamedsamiromar@users.noreply.github.com>
2022-05-30 20:13:23 +0200

    enhc in form

--- forms.py
+++ forms.py
@@ -1,6 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import (StringField, PasswordField, SubmitField,
-                     BooleanField)
+from wtforms import (StringField, PasswordField, SubmitField,BooleanField)
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
@@ -9,14 +8,11 @@ class RegistrationForm(FlaskForm):
                            validators=[DataRequired(),
                                        Length(min=2, max=20)])
     email = StringField('Email',
-                        validators=[DataRequired(),
-                                    Email()])
+                        validators=[DataRequired(), Email()])
     password = PasswordField('Password',
                              validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password',
-                                     validators=[DataRequired(),
-                                                 EqualTo('password')])
-    submit = SubmitField('Sign Up')
+                                     validators=[DataRequired(), EqualTo('password')])
 
 
 class LoginForm(FlaskForm):
@@ -26,4 +22,3 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password',
                              validators=[DataRequired()])
     remember = BooleanField('Remember Me')
-    submit = SubmitField('Login')

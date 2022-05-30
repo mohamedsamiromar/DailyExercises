f50bee3b4ef0a12cfa2814798d22176d5aa50606
mohamed samir <56839321+mohamedsamiromar@users.noreply.github.com>
2022-05-30 20:33:27 +0200

    generate a secret key

--- models.py
+++ models.py
@@ -1,17 +1,35 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import  LoginManager
+from flask_login import LoginManager
 from flask_migrate import Migrate
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 from flask_login import UserMixin
+from flask_wtf.csrf import CSRFProtect
+from flask_bcrypt import Bcrypt
+import os
 
+app = Flask(__name__, template_folder='../templates')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+bcrypt = Bcrypt(app)
+
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
+csrf = CSRFProtect(app)
 
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
 @login_manager.user_loader

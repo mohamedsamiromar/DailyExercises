392e00923eea47b62e9f3c3676fb4981414d9222
mohamed samir <56839321+mohamedsamiromar@users.noreply.github.com>
2022-06-15 23:09:35 +0200

    add new schema

--- models.py
+++ models.py
@@ -21,7 +21,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(300), nullable=False, unique=True)
+    password = db.Column(db.String(50), nullable=False, unique=True)
 
     def __repr__(self):
         return self.username

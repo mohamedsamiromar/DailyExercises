f99af0a4e082181fc122dbbf5d927e4bcdc28941
mohamed samir <56839321+mohamedsamiromar@users.noreply.github.com>
2022-06-18 20:11:43 +0200

    Fix(User Model) fix user model and init migrate

--- app.py
+++ app.py
@@ -1,118 +1,51 @@
 from flask import Flask
-from flask import request
-from flask import render_template
-from werkzeug.routing import BuildError
 
-from forms import register_form
-from models import User
-from flask import (
-    Flask,
-    render_template,
-    redirect,
-    flash,
-    url_for,
-    session
-)
 app = Flask(__name__)
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-from flask import Flask
+
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
-
-from flask_login import (
-    UserMixin,
-    login_user,
-    LoginManager,
-    current_user,
-    logout_user,
-    login_required,
-)
-
-login_manager = LoginManager()
-login_manager.session_protection = "strong"
-login_manager.login_view = "login"
-login_manager.login_message_category = "info"
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-
 db = SQLAlchemy()
-migrate = Migrate()
+migrate = Migrate(app, db)
 bcrypt = Bcrypt()
+db.init_app(app)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from datetime import datetime
+from flask_login import UserMixin
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+db = SQLAlchemy(app)
 
 
-def create_app():
-    app = Flask(__name__)
+class User(UserMixin, db.Model):
+    __tablename__ = "user"
 
-    app.secret_key = 'secret-key'
-    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(50), nullable=False, unique=True)
 
-    login_manager.init_app(app)
-    db.init_app(app)
-    migrate.init_app(app, db)
-    bcrypt.init_app(app)
-
-    return app
+    def __repr__(self):
+        return self.username
 
 
-from sqlalchemy.exc import (
-    IntegrityError,
-    DataError,
-    DatabaseError,
-    InterfaceError,
-    InvalidRequestError,
-)
+class DailyExercise(db.Model):
+    __tablename__ = "daily_exercise"
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    exercise_name = db.Column(db.String, nullable=False)
+    exercise_start_date = db.Column(db.DateTime, default=datetime.utcnow())
+    exercise_end_date = db.Column(db.DateTime, nullable=False)
+
+    def __repr__(self):
+        return self.exercise_name
 
 
 @app.route('/')
 def index():
     return 'Hello, World'
-
-
-@app.route("/register/", methods=("GET", "POST"), strict_slashes=False)
-def register():
-    form = register_form()
-    if form.validate_on_submit():
-        try:
-            email = form.email.data
-            pwd = form.pwd.data
-            username = form.username.data
-
-            newuser = User(
-                username=username,
-                email=email,
-                pwd=bcrypt.generate_password_hash(pwd),
-            )
-
-            db.session.add(newuser)
-            db.session.commit()
-            flash(f"Account Succesfully created", "success")
-            return redirect(url_for("login"))
-
-        except InvalidRequestError:
-            db.session.rollback()
-            flash(f"Something went wrong!", "danger")
-        except IntegrityError:
-            db.session.rollback()
-            flash(f"User already exists!.", "warning")
-        except DataError:
-            db.session.rollback()
-            flash(f"Invalid Entry", "warning")
-        except InterfaceError:
-            db.session.rollback()
-            flash(f"Error connecting to the database", "danger")
-        except DatabaseError:
-            db.session.rollback()
-            flash(f"Error connecting to the database", "danger")
-        except BuildError:
-            db.session.rollback()
-            flash(f"An error occured !", "danger")
-    return render_template("auth.html",
-                           form=form,
-                           text="Create account",
-                           title="Register",
-                           btn_action="Register account"
-                           )
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -1,40 +1,35 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
-from flask_migrate import Migrate
-from datetime import datetime
-from flask_login import UserMixin
-
-app = Flask(__name__, template_folder='../templates')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
-login_manager.login_message_category = 'info'
-
-
-class User(UserMixin, db.Model):
-    __tablename__ = "user"
-
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(50), nullable=False, unique=True)
-
-    def __repr__(self):
-        return self.username
-
-
-class DailyExercise(db.Model):
-    __tablename__ = "daily_exercise"
-
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
-    exercise_name = db.Column(db.String, nullable=False)
-    exercise_start_date = db.Column(db.DateTime, default=datetime.utcnow())
-    exercise_end_date = db.Column(db.DateTime, nullable=False)
-
-    def __repr__(self):
-        return self.exercise_name
+# from flask import Flask
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_login import LoginManager
+# from flask_migrate import Migrate
+# from datetime import datetime
+# from flask_login import UserMixin
+# from app import  app
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+# db = SQLAlchemy(app)
+# migrate = Migrate(db, app)
+#
+#
+# class User(UserMixin, db.Model):
+#     __tablename__ = "user"
+#
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(80), unique=True, nullable=False)
+#     email = db.Column(db.String(120), unique=True, nullable=False)
+#     password = db.Column(db.String(50), nullable=False, unique=True)
+#
+#     def __repr__(self):
+#         return self.username
+#
+#
+# class DailyExercise(db.Model):
+#     __tablename__ = "daily_exercise"
+#
+#     id = db.Column(db.Integer, primary_key=True)
+#     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+#     exercise_name = db.Column(db.String, nullable=False)
+#     exercise_start_date = db.Column(db.DateTime, default=datetime.utcnow())
+#     exercise_end_date = db.Column(db.DateTime, nullable=False)
+#
+#     def __repr__(self):
+#         return self.exercise_name
